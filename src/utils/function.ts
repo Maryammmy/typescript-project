b7447ca8acf2f08bf7ee93b694b42c5d5b4a152c
Maryammmy <malaksameh18112004@gmail.com>
2024-10-22 22:37:08 +0300

    fun

--- src/utils/function.ts
+++ src/utils/function.ts
@@ -5,7 +5,7 @@
  * @param {number} [limit=50] - The character limit for the slice. Defaults to 50 if not provided.
  * @returns {string} The sliced text with ellipsis if it exceeds the limit, or the original text if it's shorter.
  */
-export const textSlicer = (text:string, limit:number = 50) => {
+export const textSlicer = (text:string, limit:number = 50): string => {
     if (text.length >= limit) return `${text.slice(0, limit)} ...`;
     return text;
 };
